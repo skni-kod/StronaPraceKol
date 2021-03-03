f510f9b3cd16acc684980aa26c32794c29abcfaa
Smiercior <77210326+Smiercior@users.noreply.github.com>
2021-03-03 16:55:39 +0100

    All

---
+++ projekty_kol/test1/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.template import loader
+# Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    template = loader.get_template('index.html') # getting our template  
+    return HttpResponse(template.render()) # rendering the template in HttpResponse
+    #return render(request,'index.html')
+    
+def logowanie(request):
+    return render(request,'logowanie.html')
