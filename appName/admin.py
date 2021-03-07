89d042d0f16569db7c66b4c7be4eea4a845280c0
IMystyk <mateuszr@zampub.rzeszow.pl>
2021-03-07 17:37:31 +0100

    Some new changes
    added multiple authors to paper model, added announcement and message models, removed downloadedfile model

--- appName/admin.py
+++ appName/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import StudentClub, Paper, Review, UploadedFile, DownloadedFile
+from .models import StudentClub, Paper, Review, UploadedFile, Message, Announcement
 
 admin.site.register(StudentClub)
 admin.site.register(Paper)
 admin.site.register(Review)
 admin.site.register(UploadedFile)
-admin.site.register(DownloadedFile)
+admin.site.register(Message)
+admin.site.register(Announcement)
--- appName/models.py
+++ appName/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 
 class StudentClub(models.Model):
@@ -14,13 +16,16 @@ class StudentClub(models.Model):
 class Paper(models.Model):
     title = models.CharField(max_length=128)  # can be changed to longer
     club = models.ForeignKey(StudentClub, on_delete=models.CASCADE)
-    authors = models.CharField(max_length=128)
+    authors = models.ManyToManyField(User)
+    authors_string = models.CharField(max_length=128)
     keywords = models.CharField(max_length=64)
     description = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     add_date = models.DateTimeField(default=timezone.now)
     last_edit_date = models.DateTimeField(default=timezone.now)
-    status = models.SmallIntegerField()
+    approved = models.BooleanField()
+
+    def __str__(self):
+        return self.title
 
 
 class Review(models.Model):
@@ -28,6 +33,25 @@ class Review(models.Model):
     paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
     upload_date = models.DateTimeField(default=timezone.now)
     comment = models.TextField()
+
+    def __str__(self):
+        return self.paper.title
+
+
+# alternate Review model
+# class Review(models.Model):
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
+#     upload_date = models.DateTimeField(default=timezone.now)
+#     comment = models.TextField()
+#     originality = models.SmallIntegerField()
+#     accordance = models.SmallIntegerField()
+#     quality = models.SmallIntegerField()
+#     final_grade = models.SmallIntegerField()
+#     approved = models.BooleanField()
+#
+#     def __str__(self):
+#         return self.paper.title
 
 
 def paper_directory_path(instance, filename):
@@ -39,7 +63,26 @@ class UploadedFile(models.Model):
     file = models.FileField(upload_to=paper_directory_path)
     upload_date = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return self.paper.title
 
-class DownloadedFile(models.Model):
-    file = models.ForeignKey(UploadedFile, on_delete=models.CASCADE)
-    download_date = models.DateTimeField(default=timezone.now)
+
+class Announcement(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+
+
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
+    text = models.TextField()
+
+
+# checks if any paper has only one author (that is being deleted) and deletes them if any are found
+def check_empty_authors_relation(instance, **kwargs):
+    for paper in Paper.objects.filter(authors=instance):
+        if len(paper.authors.all()) == 1:
+            paper.delete()
+
+
+pre_delete.connect(check_empty_authors_relation, sender=User)
