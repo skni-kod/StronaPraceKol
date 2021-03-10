80621c14eb96e9c75b781cdfcd40445ba7988105
JakubPrzystasz <jakub.przystasz@gmail.com>
2021-03-10 09:18:42 +0100

    The great purge

--- README.md
+++
@@ -1,2 +0,0 @@
-# Projekt serwisu "Projekty Kół Naukowych" 
-Serwis ma służyć do obsługi zgłoszeń projetków studentów będących członkami kół naukowych
--- appName/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import StudentClub, Paper, Review, UploadedFile, DownloadedFile
-
-admin.site.register(StudentClub)
-admin.site.register(Paper)
-admin.site.register(Review)
-admin.site.register(UploadedFile)
-admin.site.register(DownloadedFile)
