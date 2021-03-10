28b2566ac87909b6a9c13d14e536bd4eb1412d84
JakubPrzystasz <jakub.przystasz@gmail.com>
2021-03-10 09:59:59 +0100

    Add new files

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import datetime
+from django.template import loader
 # Create your views here.
+
+def test(request):
+    now = datetime.datetime.now()
+    html = "It is now %s" % now
+    template = loader.get_template('users/index.xhtml')
+    context = {
+        'html': html,
+    }
+    return HttpResponse(template.render(context, request))
