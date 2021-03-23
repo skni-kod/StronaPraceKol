04acd9551e315a3293d7cfe7fb1e454cbb4f2f2e
IMystyk <mateuszr@zampub.rzeszow.pl>
2021-03-23 11:21:00 +0100

    Filters and restricted choices for student clubs

--- StronaProjektyKol/settings.py
+++ StronaProjektyKol/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'papers.apps.PapersConfig',
     'users.apps.UsersConfig',
     'django_summernote',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- papers/models.py
+++ papers/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db.models.signals import pre_delete
+from django.db.models import Q
 import os
 
 
