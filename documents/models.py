1c8032041dbb2e9d36f997f1fed6427ad203a00f
Mystyk <mateuszr@zampub.rzeszow.pl>
2022-02-07 14:46:37 +0100

    Added documents app

---
+++ documents/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import pre_delete
+from papers.models import StudentClub
+from django.utils import timezone
+import textwrap
+import os
+import re
+
+
+class Document(models.Model):
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=128)
+    club = models.ForeignKey(StudentClub, default=StudentClub.get_default_pk(), on_delete=models.SET_DEFAULT)
+    ready = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.name[0:40]}'
+
+
+def document_directory_path(instance, filename):
+    _filename = filename.split('.')
+    filename = re.sub(r'\W+', '', _filename[0])
+    filename = filename.replace(' ','_')
+    filename = textwrap.shorten(filename,width=100,placeholder='')
+    filename += f'.{_filename[-1]}'
+    return f'document_files/documentNo.{instance.document.pk}/{filename}'
+
+
+class UploadedFile(models.Model):
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    file = models.FileField(upload_to=document_directory_path, blank=True, max_length=512)
+
+    def filename(self):
+        return os.path.basename(self.file.name)
+
+
+def delete_file_with_object(instance, **kwargs):
+    """
+    Deletes files from system when UploadedFile object is deleted from database
+    :param instance: UploadedFile object (file that is being deleted)
+    :param kwargs:
+    :return:
+    """
+    instance.file.delete()
+
+
+pre_delete.connect(delete_file_with_object, sender=UploadedFile)
---
+++ documents/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.DocumentListView.as_view(), name='documentList'),
+    path('document/<int:pk>/', views.DocumentDetailView.as_view(), name='documentDetail'),
+    path('document/new/', views.DocumentCreateView.as_view(), name="documentCreate"),
+    path('document/<int:pk>/edit', views.DocumentEditView.as_view(), name="documentEdit"),
+    path('document/<int:pk>/delete/', views.DocumentDeleteView.as_view(), name='documentDelete'),
+]
