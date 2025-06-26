79230c5c552eb2a5161385ee1a87e590fea513c9
Filip Kula <f.kula@1ins.pl>
2025-06-26 11:23:36 +0200

    fix: handle polish signs in files name

--- papers/views.py
+++ papers/views.py
@@ -14,6 +14,7 @@ from django.urls import reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
 from django.views.static import serve
+from urllib.parse import unquote
 from StronaProjektyKol.settings import SITE_NAME, BASE_DIR, SITE_ADMIN_MAIL
 from .filters import PaperFilter
 from .forms import *
@@ -125,7 +126,8 @@ def paper_file_download(request, pk, item):
     if request.user == paper.author or request.user.groups.filter(
             name='reviewer').exists() or request.user.is_staff:
         document = UploadedFile.objects.get(pk=item)
-        filepath = str(BASE_DIR)+document.file.url
+        decoded_file_url = unquote(document.file.url)
+        filepath = str(BASE_DIR) + decoded_file_url
         return serve(request, os.path.basename(filepath), os.path.dirname(filepath))
     else:
         return redirect('paper-list')
