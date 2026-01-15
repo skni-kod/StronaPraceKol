abf5816a54f5ddff1cf68f4ba71b43b691e26db4
Norbert Mokrzycki <quostek12@gmail.com>
2026-01-15 20:44:55 +0100

    feat: #fix/mail-notifications

--- papers/apps.py
+++ papers/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class PapersConfig(AppConfig):
     name = 'papers'
+
+    def ready(self):
+        from .tasks import setup_scheduled_tasks
+        setup_scheduled_tasks()
